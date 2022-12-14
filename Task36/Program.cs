5a6f29d09327ada90e19f6b5803ebf7cf515edd5
Gorik <gosha_qwerty@rambler.ru>
2022-12-15 01:58:13 +0800

    Task 36

---
+++ Task36/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int[] Array = new int[5]; 
+int count = 0;
+
+{
+for(int i = 0; i < 5; i++)
+{
+   Array[i] = new Random().Next(10,50);
+}
+Console.WriteLine($"{string.Join(",", Array)}");
+
+for(int i = 1; i < 5; i+=2)
+{ 
+   
+   count = count + Array[i];
+}
+}
+Console.Write(count);
