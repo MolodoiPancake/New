dc27097305b2112e8448229dc33fcd69f369aa03
Gorik <gosha_qwerty@rambler.ru>
2022-12-23 00:18:54 +0800

    Task 43

---
+++ Task43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите k1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+Console.WriteLine($"Две прямые пересекаются в точке с координатами X {x}; Y {y}");
