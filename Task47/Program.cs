8ecec6b989b1bbcf8599c2aebc0980c8237f15cb
Gorik <gosha_qwerty@rambler.ru>
2022-12-27 14:16:28 +0800

    bom bom

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,11 +1,10 @@
-﻿
-void ArrayWithRandomNumbers(double[,] array)
+﻿void ArrayWithRandomNumbers(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(-100, 100) / 10;
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
         }
     }
 }
