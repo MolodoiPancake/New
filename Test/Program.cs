f71858eab194fc2c67f3bb2958e286076724a329
Gorik <gosha_qwerty@rambler.ru>
2023-01-31 21:01:29 +0800

    Напсао программу

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] FirstArray = new string[5] {"1234", "2", "hello", "world", ":3"};
+string[] SecondArray = new string[FirstArray.Length];
+void SecondArrayWithIF(string[] FirstArray, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+    if(FirstArray[i].Length <= 3)
+        {
+        SecondArray[count] = FirstArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+SecondArrayWithIF(FirstArray, SecondArray);
+PrintArray(SecondArray);
