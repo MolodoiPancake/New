63c9b62248b4c455bcea7afe9547dd421f73980e
Gorik <gosha_qwerty@rambler.ru>
2023-03-03 16:11:07 +0800

    абоба

---
+++ 1/Task36/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task 36
+Console.Clear();
+Console.Write("");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[n]; 
+int count = 0;
+
+{
+for(int i = 0; i < Array.Length; i++)
+{
+   Array[i] = new Random().Next(10,50);
+}
+Console.WriteLine($"{string.Join(",", Array)}");
+
+for(int i = 1; i < 5; i+=2)
+{ 
+   
+   count = count + Array[i];
+}
+}
+Console.Write(count);
---
+++ 1/Task66/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое значение ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение ");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 0 + n; i <= m; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine(sum);
