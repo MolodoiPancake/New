94ba9a05202d7abdf124199ba796b875700d4a1a
Gorik <gosha_qwerty@rambler.ru>
2023-03-14 22:30:17 +0800

    task 32

--- Python/lesson_6/Task32.py
+++ Python/lesson_6/Task32.py
@@ -1,4 +1,6 @@
-n = int(input('Input n ')) 
-x = int(input('Input a[0] '))
-d = int(input('Input d ')) 
-print(*range(x, x + d * n, d))
+list_1 = [3, 5, 1, 7, -4, 0, -1, 9, 6, -8]
+min_number = int(input('Введите максимальное число '))
+max_number = int(input('Введите минимальное число '))
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
