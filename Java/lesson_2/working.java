2ec09d115842b71037b94d7cd0e97895fdd50ab6
Gorik <gosha_qwerty@rambler.ru>
2023-04-10 00:03:36 +0800

    Task1

--- Java/lesson_2/working.java
+++ Java/lesson_2/working.java
@@ -1,17 +1,21 @@
 package Java.lesson_2;
 
-public class working {
-    public static void main(String[] args) {
-            System.out.println();
-    }
+import java.util.Scanner;
 
-    static String str(int number, String firstChar, String secondChar) {
-        StringBuilder result = new StringBuilder();
-        String twoChars = firstChar + secondChar;
-        for (int i = 0; i < number / 2; i++) {
-            result.append(twoChars);
-            return result.toString();
+public class working {
+public static void main(String[] args) {
+        System.out.println("Введите число -->  ");
+        Scanner sc = new Scanner(System.in);
+        int last_num = sc.nextInt();
+        sc.close();
+        int result_sum = 0;
+        int result_mult = 1;
+        for (int i = 1; i <= last_num; i++){
+            result_sum += i;
+            result_mult *= i;
         }
+        System.out.println("Treugolnoe chislo = " + result_sum);
+        System.out.println("n! = " + result_mult);
     }
 
 }
