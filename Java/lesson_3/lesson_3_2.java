a70b87bf36ab0f18831d7cfd0b0131e78b613d8e
Gorik <gosha_qwerty@rambler.ru>
2023-04-17 03:20:24 +0800

    Task 2

---
+++ Java/lesson_3/lesson_3_2.java
@@ -0,0 +1,30 @@
+package Java.lesson_3;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class lesson_3_2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Random rn  = new Random();
+        for(int i = 0; i <= 20; i ++){
+            list.add(rn.nextInt(30));
+        }
+        System.out.println("Дан список: \n" + list);
+        int min = list.get(0);
+        int max = list.get(0);
+        double sum  = 0;
+        for (int i = 0; i < list.size(); i ++){
+            if(list.get(i) < min){
+                min = list.get(i);
+            }
+            if(list.get(i) > max){
+                max = list.get(i);
+            }
+            sum += list.get(i);
+        }
+        System.out.println("Минимальное значение: " + min);
+        System.out.println("Максимальное число: "+ max);
+        System.out.println("Среднее арифметическое из списка: " + sum/list.size());
+    }
+}
