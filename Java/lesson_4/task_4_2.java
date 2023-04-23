5d19d0c65ed4be467099767cff954a3e8df1bdcd
Gorik <gosha_qwerty@rambler.ru>
2023-04-24 01:50:03 +0800

    task 2

---
+++ Java/lesson_4/task_4_2.java
@@ -0,0 +1,38 @@
+package Java.lesson_4;
+
+import java.util.LinkedList;
+
+public class task_4_2 {
+    public static void main(String[] args) {
+        LinkedList<Integer> dq = new LinkedList<>();
+
+        enqueue(dq, 1);
+        enqueue(dq, 2);
+        enqueue(dq, 3);
+        enqueue(dq, 4);
+        enqueue(dq, 5);
+        System.out.println(dq);
+
+        System.out.println(dequeue(dq));
+        System.out.println(dq);
+
+        System.out.println(first(dq));
+        System.out.println(dq);
+
+    }
+
+    public static void enqueue(LinkedList<Integer> ll, int num) {
+        ll.addLast(num);
+    }
+
+    public static int dequeue(LinkedList<Integer> ll) {
+        int num = ll.get(0);
+        ll.remove(0);
+        return num;
+    }
+
+    public static int first(LinkedList<Integer> ll) {
+        int num = ll.get(0);
+        return num;
+    }
+}
